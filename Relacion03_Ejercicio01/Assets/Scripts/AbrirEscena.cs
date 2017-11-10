171b9aa3306a52eb90ba80ef082a71399e67992f
zhuoIC <nicolimonar@gmail.com>
2017-11-10 10:07:45 +0100

    Iniciado ejercicio del menú y las escenas.

---
+++ Relacion03_Ejercicio01/Assets/Scripts/AbrirEscena.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//-----------------------------------
+using UnityEngine.SceneManagement;
+
+public class AbrirEscena : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PulsarBoton(){
+		switch (transform.gameObject.tag) {
+		case "btn1":
+			Abrir ("Escena1");
+			break;
+		case "btn2":
+			Abrir ("Escena2");
+			break;
+		case "btn3":
+			Abrir ("Escena3");
+			break;
+		case "btn4":
+			Abrir ("Escena4");
+			break;
+		default:
+			break;
+		}
+	}
+
+	void Abrir(string escena){
+		SceneManager.LoadScene (escena);
+	}
+}
