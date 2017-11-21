6b1b0a2b931597aba7e9c69837d875585323139a
zhuoIC <nicolimonar@gmail.com>
2017-11-21 07:24:03 +0100

    Añadido proyecyo MotoresyFisicas

---
+++ MotoresYfisicas/Assets/Fisica.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fisica : MonoBehaviour {
+
+	public float _velocidad = 50F;
+	public Rigidbody _rigidBody;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		Vector3 fuerzaEmpuje = new Vector3 (Input.GetAxis ("Horizontal") * _velocidad, 0, Input.GetAxis ("Vertical")*_velocidad);
+		_rigidBody.AddForce (fuerzaEmpuje);
+		_rigidBody.freezeRotation = true;
+	}
+}
---
+++ MotoresYfisicas/Assets/Trasladar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trasladar : MonoBehaviour {
+	public Transform _origen;
+	public Transform _direccion;
+	float _velocidad = 0.01F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_origen.Translate (_direccion.transform.position * _velocidad);
+	}
+}
